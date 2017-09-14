eacfd5be516e7678da226459dbad69fa6d45c485
yanbinbi <18244984914@163.com>
2017-09-14 21:17:52 +0800

    model设计

--- product_manage/models.py
+++ product_manage/models.py
@@ -7,7 +7,15 @@ class User(models.Model):
     email = models.EmailField("邮箱")
     phone = models.IntegerField("手机号", default=0000000000)
     password = models.CharField("密码", max_length=255)
+    nickname = models.CharField(max_length=40)
+    name = models.CharField("姓名")
 
+
+
+# 地址类
+class Address(models.Model):
+    content = models.TextField("地址", max_length=255)
+    user = models.ForeignKey(User, verbose_name="用户")
 
 # 商品
 class Product(models.Model):
