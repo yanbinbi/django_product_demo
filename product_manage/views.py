84cbf35791a34ad0cd5a60fe7c7983db2e8d9cbf
yanbinbi <18244984914@163.com>
2017-09-15 11:00:52 +0800

    引入login

--- product_manage/views.py
+++ product_manage/views.py
@@ -32,6 +32,7 @@ def email_register(request):
                 user = User.objects.create(email=email_register_form.cleaned_data["email"],
                                            password=make_password(email_register_form.cleaned_data["password"], 'ybb', 'pbkdf2_sha256'))
                 user.save()
+                login(request, user)
                 return render(request, "index.html", locals())
             else:
                 return render(request, "failure.html", {"reason": email_register_form.errors})
@@ -54,7 +55,6 @@ def phone_register(request):
                                            password=phone_register_form.cleaned_data['password'])
                 user.save()
                 # django指定的登录方式
-                user.backend = "django.contrib.auth.backends.ModelBackend"
                 login(request, user)
             else:
                 return render(request, "failure.html", {"reason": phone_register_form.errors})
@@ -77,11 +77,13 @@ def do_login(request):
 
                 # 判断获得的username是邮箱还是手机号码
                 user = User.objects.filter(email__exact=username, password__exact=make_password(password, 'ybb', 'pbkdf2_sha256'))
+                # user = authenticate(request, username=username, password=password)
                 if user is None:
                     user = User.objects.filter(phone__exact=username, password__exact=make_password(password, 'ybb', 'pbkdf2_sha256'))
                 # 如果用户存在
                 if user is not None:
                     # 登录成功，页面跳转到个人中心
+                    login(request, user)
                     return render(request, "index.html", locals())
                 else:
                     return render(request, 'failure.html', {'reason': '登录验证失败'})
@@ -93,3 +95,10 @@ def do_login(request):
         logger.error(e)
         print(e)
     return render(request, "login.html", locals())
+
+# 注销
+def do_logout(request):
+    try:
+        logout(request)
+    except Exception as e:
+        logger.error(e)
